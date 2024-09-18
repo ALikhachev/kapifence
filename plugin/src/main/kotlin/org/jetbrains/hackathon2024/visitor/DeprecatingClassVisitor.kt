801a28607b6f238bea66a4988f914215b5165718
Dmitrii Krasnov <dmitrii.krasnov@jetbrains.com>
2024-09-18 21:01:53 +0200

    fixup! Fixed field test

--- plugin/src/main/kotlin/org/jetbrains/hackathon2024/visitor/DeprecatingClassVisitor.kt
+++ plugin/src/main/kotlin/org/jetbrains/hackathon2024/visitor/DeprecatingClassVisitor.kt
@@ -38,68 +38,20 @@ class DeprecatingClassVisitor(
     }
 
     override fun visitEnd() {
+        val metadata = kotlinMetadata
+        if (metadata is KotlinClassMetadata.Class) {
+            // TODO: file facades?
+            val kClass = metadata.kmClass
+            kClass.hasAnnotations = true
+        }
+
+        writeAnnotation(metadata.write())
         super.visitEnd()
         if (!isAlreadyDeprecated && shouldApplyChanges) {
             // don't deprecate and don't override the message if the class is already deprecated
             val deprecatedAnnotation = super.visitAnnotation(DEPRECATED_ANNOTATION_DESC, true)
             deprecatedAnnotation.visit("message", deprecationMessage)
             deprecatedAnnotation.visitEnd()
-        }
-    }
-
-    /**
-     * Reads the existing [Metadata] and overrides it.
-     * Does not define the super annotation visitor and does not contain calls to `super`,
-     * because we don't need to write the existing annotation as is.
-     *
-     * The kotlin.Metadata of the supported versions won't change and has only primitive and array values,
-     * thus we override only [visit] and [visitArray].
-     */
-    private inner class MetadataAnnotationVisitor : AnnotationVisitor(Opcodes.ASM9) {
-        private val values = mutableMapOf<String, Any>()
-
-        override fun visit(name: String, value: Any?) {
-            if (value != null) {
-                values[name] = value
-            }
-        }
-
-        override fun visitArray(name: String): AnnotationVisitor {
-            return object : AnnotationVisitor(Opcodes.ASM9) {
-                private val list = mutableListOf<String>()
-
-                override fun visit(name: String?, value: Any?) {
-                    if (value != null) {
-                        list.add(value as String)
-                    }
-                }
-
-                override fun visitEnd() {
-                    values[name] = list.toTypedArray()
-                }
-            }
-        }
-
-        @Suppress("UNCHECKED_CAST")
-        override fun visitEnd() {
-            val header = Metadata(
-                kind = values["k"] as Int? ?: 1,
-                metadataVersion = values["mv"] as IntArray? ?: intArrayOf(),
-                data1 = values["d1"] as Array<String>? ?: emptyArray(),
-                data2 = values["d2"] as Array<String>? ?: emptyArray(),
-                extraString = values["xs"] as String? ?: "",
-                packageName = values["pn"] as String? ?: "",
-                extraInt = values["xi"] as Int? ?: 0
-            )
-
-            val metadata = KotlinClassMetadata.Companion.readStrict(header)
-            if (metadata is KotlinClassMetadata.Class) {
-                // TODO: file facades?
-                val kClass = metadata.kmClass
-                kClass.hasAnnotations = true
-            }
-
-            writeAnnotation(metadata.write())
         }
     }
 }
